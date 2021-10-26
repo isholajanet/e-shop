1bc24b1a921f0f869e34e3e7150a4b4a13560bbe
Janet Ishola <janetishola2010@gmail.com>
2021-10-26 12:50:22 +0100

    update product implementation

--- src/main/java/com/ecommerce/shop/service/ProductServiceImpl.java
+++ src/main/java/com/ecommerce/shop/service/ProductServiceImpl.java
@@ -1,11 +1,13 @@
 package com.ecommerce.shop.service;
 
 import com.ecommerce.shop.data.model.Product;
+import com.ecommerce.shop.data.model.ProductDto;
 import com.ecommerce.shop.data.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductServiceImpl implements ProductService{
@@ -30,5 +32,57 @@ public class ProductServiceImpl implements ProductService{
     @Override
     public void deleteById(Long id) {
         productRepository.deleteById(id);
+    }
+
+    @Override
+    public Product update(Long id, ProductDto update) {
+        Optional<Product> foundProduct = productRepository.findById(id);
+
+
+        if(foundProduct.isPresent()){
+            Product productToUpdate = foundProduct.get();
+            boolean isNameValid = update.getName() != null && !update.getName().isEmpty() && !update.getName().isBlank()
+                    && !update.getName().equals(productToUpdate.getName());
+            boolean isPriceValid = update.getPrice() != null  && update.getPrice() != productToUpdate.getPrice();
+            boolean isCurrencyValid = update.getCurrency() != null && !update.getCurrency().equals(productToUpdate.getCurrency());
+            boolean isDetailsValid = update.getDetails() != null && !update.getDetails().isEmpty() && !update.getDetails().isBlank()
+                    && !update.getDetails().equals(productToUpdate.getDetails());
+            boolean isImageUrlValid = update.getImageUrl() != null && !update.getImageUrl().isEmpty()
+                    && !update.getImageUrl().equals(productToUpdate.getImageUrl());
+
+            if(isNameValid){
+                productToUpdate.setName(update.getName());
+            }else{
+                productToUpdate.setName(productToUpdate.getName());
+            }
+
+            if(isPriceValid){
+                productToUpdate.setPrice(update.getPrice());
+            }else{
+                productToUpdate.setPrice(productToUpdate.getPrice());
+            }
+
+            if(isCurrencyValid){
+                productToUpdate.setCurrency(update.getCurrency());
+            }else{
+                productToUpdate.setCurrency(productToUpdate.getCurrency());
+            }
+
+            if(isDetailsValid){
+                productToUpdate.setDetails(update.getDetails());
+            }else{
+                productToUpdate.setDetails(productToUpdate.getDetails());
+            }
+
+            if(isImageUrlValid){
+                productToUpdate.setImageUrl(update.getImageUrl());
+            }else{
+                productToUpdate.setImageUrl(productToUpdate.getImageUrl());
+            }
+            return productRepository.save(productToUpdate);
+        }else{
+            throw new IllegalArgumentException("Incorrect Id");
+        }
+
     }
 }
--- src/main/java/com/ecommerce/shop/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/shop/web/controller/ProductController.java
@@ -1,9 +1,12 @@
 package com.ecommerce.shop.web.controller;
 
 import com.ecommerce.shop.data.model.Product;
+import com.ecommerce.shop.data.model.ProductDto;
 import com.ecommerce.shop.service.ProductService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -31,5 +34,14 @@ public class ProductController {
 
         return ResponseEntity.noContent().build();
     }
+    @PatchMapping("/{id}")
+    public ResponseEntity<?> updateProduct(@PathVariable Long id, @RequestBody ProductDto update){
+        try {
+            productServiceImpl.update(id, update);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }catch(NullPointerException e){
+            return new ResponseEntity<>(e.getLocalizedMessage(), HttpStatus.BAD_REQUEST);
+        }
 
+    }
 }
